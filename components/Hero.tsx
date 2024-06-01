fc19f054a5d9d49151d0dbf44ffe54e27907360d
jatin chawla <jatinchawla682@gmail.com>
2024-06-01 16:32:00 +0530

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <Spotlight />
+    </div>
+  );
+};
+
+export default Hero;
