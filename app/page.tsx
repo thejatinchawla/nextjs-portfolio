67a2f5595f2ffe1300c4fecd3240fe5b271104a1
jatin chawla <jatinchawla682@gmail.com>
2024-06-05 01:09:50 +0530

    Added metadata and experience component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/ReacentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,6 +14,8 @@ export default function Home() {
       <Hero />
       <Grid />
       <RecentProjects />
+      <Clients />
+      <Experience />
     </main>
   );
 }
