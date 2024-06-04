1ea64bbef8f55db3e33931dcddf74f8e64937f3e
jatin chawla <jatinchawla682@gmail.com>
2024-06-04 23:15:32 +0530

    Added assets and other components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/ReacentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto m:px-10 px-5">
       <div className="max-w-7xl w-full"></div>
+      <FloatingNav navItems={navItems} />
       <Hero />
+      <Grid />
+      <RecentProjects />
     </main>
   );
 }
